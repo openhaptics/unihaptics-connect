cae7891d3a5956332fda7ec375cc957392c6dbf5
Leonid Meleshin <lm@devolt.one>
2022-12-05 23:45:46 +0400

    feat: initialize windows app
    `npx react-native-windows-init --overwrite`

---
+++ windows/ohc/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::ohc::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::ohc::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
